095ca6a506e320ae629972c5912f73d1806fc69e
BasilDaboor <Basildaboor12@gmail.com>
2025-03-25 02:10:53 +0300

    create the user and provider migrate

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function provider()
+    {
+        return $this->hasOne(Provider::class);
+    }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->enum('role', ['customer', 'provider', 'admin', 'super_admin'])->default('customer'); // Role as ENUM
             $table->rememberToken();
             $table->timestamps();
         });
